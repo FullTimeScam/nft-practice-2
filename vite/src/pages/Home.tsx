3a947575d32e6f2dff9b90bcfbf2d28044a908f8
FullTimeScam <418.gingin@gmail.com>
2024-06-10 18:54:07 +0900

    메인사진에 링크 추가

--- vite/src/pages/Home.tsx
+++ vite/src/pages/Home.tsx
@@ -1,16 +1,19 @@
 import { Flex, Image } from "@chakra-ui/react";
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
 const Home: FC = () => {
   return (
     <Flex width="100vw" height="100vh">
-      <Image
-        src="/images/KD_5.png"
-        alt="Home Image"
-        objectFit="cover"
-        width="100%"
-        height="100%"
-      />
+      <Link to="/sale-nft">
+        <Image
+          src="/images/KD_5.png"
+          alt="Home Image"
+          objectFit="cover"
+          width="100%"
+          height="100%"
+        />
+      </Link>
     </Flex>
   );
 };
