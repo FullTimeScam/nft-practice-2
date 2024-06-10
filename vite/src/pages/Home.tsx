44b111b23e131f3ed951fce45fdaf69a98f7936d
FullTimeScam <418.gingin@gmail.com>
2024-06-10 18:15:23 +0900

    메인페이지 추가

--- vite/src/pages/Home.tsx
+++ vite/src/pages/Home.tsx
@@ -1,8 +1,18 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import { FC } from "react";
 
 const Home: FC = () => {
-  return <Flex>Home</Flex>;
+  return (
+    <Flex width="100vw" height="100vh">
+      <Image
+        src="/public/images/KD_5.png"
+        alt="Home Image"
+        objectFit="cover"
+        width="100%"
+        height="100%"
+      />
+    </Flex>
+  );
 };
 
 export default Home;
