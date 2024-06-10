ae0942bcebfd3dccf70da32b42de95e366402d07
FullTimeScam <418.gingin@gmail.com>
2024-06-10 16:49:04 +0900

    마켓 내 배경색 제거

--- vite/src/pages/SaleNft.tsx
+++ vite/src/pages/SaleNft.tsx
@@ -59,15 +59,7 @@ const SaleNft: FC = () => {
   useEffect(() => console.log(nftMetadataArray), [nftMetadataArray]);
 
   return (
-    <Flex
-      bgColor="red.100"
-      w="100%"
-      alignItems="center"
-      flexDir="column"
-      gap={2}
-      mt={8}
-      mb={20}
-    >
+    <Flex w="100%" alignItems="center" flexDir="column" gap={2} mt={8} mb={20}>
       {signer ? (
         <Grid
           templateColumns={[
